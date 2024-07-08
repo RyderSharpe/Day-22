9aa2ed15154729f41cf5c86148f2c95e4df02bc6
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-07-08 03:48:12 -0700

    ball.py

--- ball.py
+++ ball.py
@@ -3,10 +3,11 @@ from turtle import Turtle
 # Constants
 WIDTH = 1
 HEIGHT = 1   # All turtles start out as 20 x 20, therefore width to be stretched by 5 to achieve 100 pixels
-
-BALL_SPEED = 3
+BALL_X_SPEED = 3
+BALL_Y_SPEED = 1
 
 class Ball(Turtle):
+
 
     def __init__(self, xpos, ypos):
         super().__init__()
@@ -15,17 +16,34 @@ class Ball(Turtle):
         self.color("white")
         self.penup()
         self.goto(xpos, ypos)
+        self.x_move = BALL_X_SPEED
+        self.y_move = BALL_Y_SPEED
+        self.move_speed = 0.1
+
 
     def ball_move(self):
-        # Get current coordinates
-        current_x = self.xcor()
-        current_y = self.ycor()
-
         # Update x and y coordinates for diagonal movement (adjust values for desired speed)
-        new_x = current_x + BALL_SPEED  # Move right
-        new_y = current_y + BALL_SPEED  # Move up
-
+        new_x = self.xcor() + self.x_move  # Move right
+        new_y = self.ycor() + self.y_move  # Move up
         # Update the ball's position
         self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.009
+
+    def out_of_bounds(self):
+        # Ball resets to middle
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.x_move *= -1
+
+    # def speed(self):
+    #     self.x_move += 1
+    #     self.y_move += 0.5
+
 
 
